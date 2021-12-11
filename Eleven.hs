b774b78a4b66c3a207f64b0a6030d037a5c05af8
Angus Findlay <angusf@theodo.co.uk>
2021-12-11 21:48:47 +0000

    refactor day 11

--- Eleven.hs
+++ Eleven.hs
@@ -3,41 +3,27 @@ import Data.Maybe
 import Data.Char
 import Debug.Trace
 import qualified Data.Map as M
-import Data.Array
+import Data.Array hiding (index)
 import Control.Concurrent
 import System.IO.Unsafe
 
-c = 100
-
-demo = pt2 <$> readFile "demo10.txt"
-test = pt2 <$> readFile "test10.txt"
-main = (show . pt2 <$> readFile "input10.txt") >>= putStrLn
+main = pt2 <$> readFile "input10.txt"
 
 data Cell
-  = Zero 
-  | One 
-  | Two 
-  | Three 
-  | Four 
-  | Five 
-  | Six 
-  | Seven 
-  | Eight 
-  | Nine 
-  | Exploding 
-  | Exploded 
-    deriving (Eq)
+  = Zero | One | Two | Three | Four
+  | Five | Six | Seven | Eight | Nine 
+  | Exploding | Exploded deriving (Eq)
 
-intToCell 0 = Zero 
-intToCell 1 = One 
-intToCell 2 = Two 
-intToCell 3 = Three 
-intToCell 4 = Four 
-intToCell 5 = Five 
-intToCell 6 = Six 
-intToCell 7 = Seven 
-intToCell 8 = Eight 
-intToCell 9 = Nine 
+toCell '0' = Zero 
+toCell '1' = One 
+toCell '2' = Two 
+toCell '3' = Three 
+toCell '4' = Four 
+toCell '5' = Five 
+toCell '6' = Six 
+toCell '7' = Seven 
+toCell '8' = Eight 
+toCell '9' = Nine 
 
 inc Zero      = One 
 inc One       = Two 
@@ -52,74 +38,45 @@ inc Nine      = Exploding
 inc Exploding = Exploding
 inc Exploded  = Exploded
 
-toChar Zero  = '◌'
-toChar One   = '◜'
-toChar Two   = '◝'
-toChar Three = '◠' 
-toChar Four  = '◯'
-toChar Five  = '◷'
-toChar Six   = '◔'
-toChar Seven = '◑' 
-toChar Eight = '◕' 
-toChar Nine  = '●'
-toChar Exploding  = '?'
-toChar Exploded  = '◌'
+type Grid = Array (Int, Int) Cell
 
-toString :: [[Cell]] -> String
-toString = concatMap (\row -> (intersperse ' ' (map toChar row)) ++ "\n")
+pt2 = fmap fst . find (all (== Zero) . snd) . zip [0..] . iterate step . fmap toCell . to2dArray . lines
 
-pt2 = getIndexWhere allFlashing . iterate step . map (map (intToCell . digitToInt)) . lines
+step :: Grid -> Grid
+step = cascade . fmap inc
 
-allFlashing xs = all (== Zero) $ concat xs
-
-getIndexWhere f grids = helper f 0 grids
-
-helper f n (grid:more) = if f grid then n else helper f (n + 1) more
-
-step :: [[Cell]] -> [[Cell]]
-step grid = cascade $ map (map inc) grid
-
-cascade :: [[Cell]] -> [[Cell]]
+cascade :: Grid -> Grid
 cascade grid =
-    if any (== Exploding) (concat grid) then
-        cascade (cascadeFirst grid)
+    case index (== Exploding) grid of
+        Just pos ->
+            cascade (explode pos grid)
+        Nothing ->
+            fmap (iff (== Exploded) Zero) grid
+
+explode :: (Int, Int) -> Grid -> Grid
+explode pos grid =
+    foldr
+        (\p acc -> at acc p inc)
+        (at grid pos (const Exploded))
+        (surrounding pos)
+
+-- helpers
+
+at xs p f =
+    if (inRange (bounds xs) p) then
+        xs // [(p, f (xs ! p))]
     else
-        log_ $ map (map (\x -> if x == Exploded then Zero else x)) grid
+        xs
 
-cascadeFirst :: [[Cell]] -> [[Cell]]
-cascadeFirst grid =
-    let
-        pos = coordsOfFirst (== Exploding) grid
-        incAt = surrounding pos
-        flashed = updateAt grid pos (const Exploded)
-        next = foldr (\p myGrid -> updateAt myGrid p inc) flashed incAt
-    in
-        next
+index f = fmap fst . find (f . snd) . assocs
 
-log_ x = thenSleep (trace (toString x ++ "\n") x)
-
-thenSleep a = const a $! unsafePerformIO (threadDelay 70000)
+to2dArray xs = listArray ((0, 0), (length (head xs) - 1, length xs - 1)) $ concat xs
 
 surrounding :: (Int, Int) -> [(Int, Int)]
 surrounding (a, b) =
-    [ (x, y) | x <- [a - 1 .. a + 1]
-             , y <- [b - 1 .. b + 1] 
-    ]
+    [ (x, y) | x <- a ± 1, y <- b ± 1 ]
 
-withCoords :: [[a]] -> [[((Int, Int), a)]]
-withCoords xs =
-    zipWith
-        (\ns y -> map (\(x, v) -> ((x, y), v)) ns)
-        (map indexed xs)
-        [0..]
+x ± d = [ x - d .. x + d ]
 
-indexed :: [a] -> [(Int, a)]
-indexed = zip [0..]
-
-coordsOfFirst :: (a -> Bool) -> [[a]] -> (Int, Int)
-coordsOfFirst f grid = head $ map fst $ filter (\(_, x) -> f x) $ concat $ withCoords grid
-
-updateAt :: [[a]] -> (Int, Int) -> (a -> a) -> [[a]]
-updateAt grid pos f = map (map update) $ withCoords grid
-    where update (p, x) = if pos == p then f x else x
-
+iff f a x | f x = a
+          | otherwise = x
